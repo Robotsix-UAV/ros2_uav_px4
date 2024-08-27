403ec7c2650493e88bde43612d43cbb91d48c0dd
Damien SIX <damien@robotsix.net>
2024-08-27 16:16:23 +0200

    ongoing

--- include/ros2_uav_px4/utils/derivative_filter.hpp
+++ include/ros2_uav_px4/utils/derivative_filter.hpp
@@ -25,15 +25,30 @@
 
 namespace uav_ros2::utils
 {
+/**
+ * @brief Butterworth filter implementation.
+ */
 class ButterworthFilter
 {
 public:
+  /**
+   * @brief Constructs a new ButterworthFilter object.
+   *
+   * @param cutoff_frequency The cutoff frequency of the filter.
+   * @param sampling_frequency The sampling frequency of the input data.
+   */
   ButterworthFilter(double cutoff_frequency, double sampling_frequency)
   : cutoff_(cutoff_frequency), fs_(sampling_frequency)
   {
     computeCoefficients();
   }
 
+  /**
+   * @brief Apply a forward-backward Butterworth filter to the input data.
+   *
+   * @param input The input data.
+   * @return std::vector<double> The filtered data.
+   */
   std::vector<double> filtfilt(const std::vector<double> & input)
   {
     std::vector<double> filtered = filter(input);
@@ -43,6 +58,12 @@ public:
     return filtered;
   }
 
+  /**
+   * @brief Apply a forward Butterworth filter to the input data.
+   *
+   * @param input The input data.
+   * @return std::vector<double> The filtered data.
+   */
   std::vector<double> filter(const std::vector<double> & input)
   {
     std::vector<double> output(input.size(), 0.0);
@@ -59,11 +80,13 @@ public:
   }
 
 private:
-  double cutoff_;
-  double fs_;
-  size_t clip_samples_;
-  double a0, a1, a2, b1, b2;
+  double cutoff_;             //< Cutoff frequency
+  double fs_;                 //< Sampling frequency
+  double a0, a1, a2, b1, b2;  //< Filter coefficients
 
+  /**
+   * @brief Compute the filter coefficients as function of the cutoff frequency and the sampling frequency.
+   */
   void computeCoefficients()
   {
     double wc = tan(M_PI * cutoff_ / fs_);
@@ -79,18 +102,36 @@ private:
   }
 };
 
-
+/**
+ * @brief Derivative filter implementation.
+ */
 class DerivativeFilter
 {
 public:
+  /**
+   * @brief Constructs a new DerivativeFilter object.
+   *
+   * @param cutoff_frequency The cutoff frequency of the filter.
+   * @param sampling_frequency The sampling frequency of the input data.
+   */
   DerivativeFilter(double cutoff_frequency, double sampling_frequency)
   : filter_(cutoff_frequency, sampling_frequency), fs_(sampling_frequency) {}
 
+  /**
+   * @brief Add a new data point to the filter.
+   *
+   * @param value The data point to add.
+   */
   void addData(double value)
   {
     data_.push_back(value);
   }
 
+  /**
+   * @brief Compute the derivative of the input data.
+   *
+   * @return std::vector<double> The derivative of the input data.
+   */
   std::vector<double> computeDerivative()
   {
     if (data_.size() < 3) {return {};}
@@ -102,6 +143,11 @@ public:
     return derivatives;
   }
 
+  /**
+   * @brief Derive and filter the input data.
+   *
+   * @return std::vector<double> The filtered derivative.
+   */
   std::vector<double> process()
   {
     auto derivatives = computeDerivative();
@@ -109,6 +155,12 @@ public:
     return filter_.filtfilt(derivatives);
   }
 
+  /**
+   * @brief Derive and filter the input data.
+   *
+   * @param derivative The derivative of the input data.
+   * @param derivative_filtered The filtered derivative.
+   */
   void process(std::vector<double> & derivative, std::vector<double> & derivative_filtered)
   {
     derivative = computeDerivative();
@@ -117,21 +169,33 @@ public:
   }
 
 private:
-  ButterworthFilter filter_;
-  double fs_;    // Sampling frequency
-  std::vector<double> data_;
+  ButterworthFilter filter_;  //< Butterworth filter
+  double fs_;                 //< Sampling frequency
+  std::vector<double> data_;  //< Input data
 };
 
-
+/**
+ * @brief 3D derivative filter implementation wrapping 3 1D derivative filters.
+ */
 class DerivativeFilter3D
 {
 public:
+  /**
+   * @brief Constructs a new DerivativeFilter3D object.
+   *
+   * @param cutoff_frequency The cutoff frequency of the filter.
+   * @param sampling_frequency The sampling frequency of the input data.
+   */
   DerivativeFilter3D(double cutoff_frequency, double sampling_frequency)
   : filter_x_(cutoff_frequency, sampling_frequency),
     filter_y_(cutoff_frequency, sampling_frequency),
     filter_z_(cutoff_frequency, sampling_frequency),
     fs_(sampling_frequency) {}
 
+  /**
+   * @brief Add a new data point to the filter.
+   * @param value The data point to add.
+   */
   void addData(const tf2::Vector3 & value)
   {
     filter_x_.addData(value.x());
@@ -139,6 +203,11 @@ public:
     filter_z_.addData(value.z());
   }
 
+  /**
+   * @brief Compute the derivative of the input data and filter it.
+   *
+   * @return std::vector<tf2::Vector3> The derivative of the input data.
+   */
   std::vector<tf2::Vector3> process()
   {
     auto x = filter_x_.process();
@@ -152,6 +221,12 @@ public:
     return derivatives;
   }
 
+  /**
+   * @brief Compute the derivative of the input data and filter it.
+   *
+   * @param derivative The derivative of the input data.
+   * @param derivative_filtered The filtered derivative.
+   */
   void process(
     std::vector<tf2::Vector3> & derivative,
     std::vector<tf2::Vector3> & derivative_filtered)
@@ -172,9 +247,9 @@ public:
   }
 
 private:
-  DerivativeFilter filter_x_;
-  DerivativeFilter filter_y_;
-  DerivativeFilter filter_z_;
-  double fs_;    // Sampling frequency
+  DerivativeFilter filter_x_;  //< Derivative filter for the x-axis
+  DerivativeFilter filter_y_;  //< Derivative filter for the y-axis
+  DerivativeFilter filter_z_;  //< Derivative filter for the z-axis
+  double fs_;                  //< Sampling frequency
 };
 }  // namespace uav_ros2::utils
