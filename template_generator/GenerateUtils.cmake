8939e187a248d300768cb10bb00f3de185b138f8
Damien SIX <damien@robotsix.net>
2024-08-30 10:09:55 +0200

    ongoing

--- template_generator/GenerateUtils.cmake
+++ template_generator/GenerateUtils.cmake
@@ -1,4 +1,18 @@
-# cmake/GenerateUtils.cmake
+# Copyright 2024 The Technology Innovation Institute (TII)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Damien Six (damien@robotsix.net)
 
 # Define the custom function for checking and generating files if needed
 function(check_and_generate_if_needed config_file output_file template_file)
